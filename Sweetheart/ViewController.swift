557720c4f5792eff4fc8f5cbf4e82ffe7ea1c117
Камалов Мурат <kamalov@nanosemantics.ai>
2021-01-27 23:19:26 +0300

    Initial Commit

---
+++ Sweetheart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sweetheart
+//
+//  Created by anmin on 27.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
